015bcf5529b064f280e04d55dd79fef411a5ef58
Arnold <urjelarnoldb@gmail.com>
2025-12-12 15:12:03 +0700

    Update API key comments for security notice
    Removed personal account reference and added 'UNSECURE - test' notes to MapTiler and Mapbox API key comments to indicate these keys are for testing purposes and not secure.

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,10 +1,10 @@
-// nanti ganti pake akun kampus/cnc, ini pake akun ku (AprilArn) dawg
+// nanti ganti pake akun kampus/cnc,
 // note: taruh file ini di 'assets/js/config.js(file ini)'
 
-// 1. https://www.maptiler.com/
+// 1. https://www.maptiler.com/ (UNSECURE - test)
 const MAPTILER_API_KEY = 'FynxEs1bILoP78wso2ZW'; 
 
-// 2. https://www.mapbox.com/
+// 2. https://www.mapbox.com/ (UNSECURE - test)
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiYXByaWxhcm4iLCJhIjoiY21oazN5emtvMWs4bTJqcHlmenpxbXE3ZSJ9.VXLNeQB60q9Xf8lj3L1PCg';
 
 // 3. Google Maps API Key
